7409d4da23cfbbf774c7f3879f0818768567cf62
caongocthien <61340843+caongocthien@users.noreply.github.com>
2024-08-27 22:39:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('cao ngoc thien');
+const core = require('@actions/core');
+
+try {
+  // Get the input 'message'
+  const message = core.getInput('message');
+  console.log(`Message: ${message}`);
+} catch (error) {
+  core.setFailed(`Action failed with error: ${error.message}`);
+}
