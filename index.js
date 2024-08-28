4dd356298c24a9c1b798cdedccdcccc20c06d5da
caongocthien <61340843+caongocthien@users.noreply.github.com>
2024-08-28 08:51:48 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 const core = require('@actions/core');
+const { promises: fs } = require('fs');
+const path = require('path');
 
-try {
-  // Get the input 'message'
-  const message = core.getInput('message');
-  console.log(`Message: ${message}`);
-} catch (error) {
-  core.setFailed(`Action failed with error: ${error.message}`);
+async function run() {
+  try {
+    // Read the package-lock.json file
+    const packageLockPath = path.join(process.cwd(), 'package-lock.json');
+    const packageLock = JSON.parse(await fs.readFile(packageLockPath, 'utf8'));
+
+    console.log('packageLock', packageLock)
+    
+    // Extract dependencies
+    const dependencies = packageLock.dependencies;
+
+
+    if (!dependencies) {
+      core.setFailed('No dependencies found in package-lock.json.');
+      return;
+    }
+
+    // Iterate over dependencies and check licenses
+    for (const [packageName, packageInfo] of Object.entries(dependencies)) {
+      const license = packageInfo.license || 'Unknown';
+      core.info(`The license for ${packageName} is ${license}`);
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+run();
