8306af203ca39a85a516cdb087877b390288db32
caongocthien <61340843+caongocthien@users.noreply.github.com>
2024-08-28 09:12:30 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,22 +8,34 @@ async function run() {
     const packageLockPath = path.join(process.cwd(), 'package-lock.json');
     const packageLock = JSON.parse(await fs.readFile(packageLockPath, 'utf8'));
 
-    console.log('packageLock', packageLock)
+    console.log('packageLock', packageLock.packages)
     
     // Extract dependencies
-    const dependencies = packageLock.dependencies;
+    const dependencies = packageLock.packages;
 
 
     if (!dependencies) {
-      core.setFailed('No dependencies found in package-lock.json.');
+      core.info('No dependencies found in package-lock.json.');
       return;
     }
 
-    // Iterate over dependencies and check licenses
+    const listNoneMITLicense = [];
+
     for (const [packageName, packageInfo] of Object.entries(dependencies)) {
-      const license = packageInfo.license || 'Unknown';
-      core.info(`The license for ${packageName} is ${license}`);
+      if (packageInfo.license !== 'MIT' || packageInfo.license !== 'ISC') {
+          listNoneMITLicense.push(packageName)
+      }
     }
+
+    // Check have package with license not suitable
+    if (listNoneMITLicense.length > 0) {
+      const formattedPackages = listNoneMITLicense.map(pkg => `- ${pkg}`).join('\n');
+      const message = `Dependencies without license suitable for project:\n${formattedPackages}`;
+      core.setFailed(message);
+    } else {
+      core.info('All dependencies have suitable licenses.');
+    }
+    
   } catch (error) {
     core.setFailed(error.message);
   }
